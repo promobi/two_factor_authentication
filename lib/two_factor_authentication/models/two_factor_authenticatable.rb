10b12802ef66376f0b6361e9631db6e0ae7231dd
Romil Mehta <romil.mehta@promobitech.com>
2025-08-21 14:43:00 +0530

    Change condition for cooling period

--- lib/two_factor_authentication/models/two_factor_authenticatable.rb
+++ lib/two_factor_authentication/models/two_factor_authenticatable.rb
@@ -100,7 +100,7 @@ module Devise
 
         def create_direct_otp(options = {})
           # Do not generate new OTP with in cooling period
-          return if direct_otp.present? && direct_otp_sent_at.present? && direct_otp_sent_at > self.class.direct_otp_valid_for.seconds.ago
+          return if direct_otp.present? && direct_otp_sent_at.present? && direct_otp_sent_at > self.class.generate_new_otp_after_seconds.seconds.ago
 
           # Create a new random OTP and store it in the database
           digits = options[:length] || self.class.direct_otp_length || 6
